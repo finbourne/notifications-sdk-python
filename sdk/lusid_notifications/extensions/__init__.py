1312595c32b72a61920e687632b0ceced51e4ca5
Concourse <concourse@finbourne.com>
2023-11-29 16:53:32 +0000

    Updates for v2.0.1-beta

--- sdk/lusid_notifications/extensions/__init__.py
+++ sdk/lusid_notifications/extensions/__init__.py
@@ -1,15 +1,7 @@
-from lusid_notifications.extensions.api_client_builder import build_client
 from lusid_notifications.extensions.api_client_factory import SyncApiClientFactory, ApiClientFactory
 from lusid_notifications.extensions.configuration_loaders import (
     ConfigurationLoader,
     SecretsFileConfigurationLoader,
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
-    get_api_configuration,
 )
-from lusid_notifications.extensions.api_configuration import ApiConfiguration
-from lusid_notifications.extensions.retry import RetryingRestWrapper, RetryingRestWrapperAsync
-from lusid_notifications.extensions.proxy_config import ProxyConfig
-from lusid_notifications.extensions.refreshing_token import RefreshingToken
-from lusid_notifications.extensions.api_client import SyncApiClient
-from lusid_notifications.extensions.rest import RESTClientObject
